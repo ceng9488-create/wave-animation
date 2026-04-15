18d995f10a9993309df6e98551253f44b1f735c3
Chueeng <eng5128@gmail.com>
2026-04-15 21:27:54 +0800

    update config

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import App from './router.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/wave-animation/">
       <App />
     </BrowserRouter>
   </StrictMode>,
